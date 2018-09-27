3a8c356cc0ff8ee5e0823b3a419cca4edef2e1f3
Mitrichone <dmitry.kirzhaev@gmail.com>
2018-09-28 00:56:16 +0400

    Add Application class

---
+++ src/RPIS61/Kirzhaev/wdad/Application.java
@@ -0,0 +1,7 @@
+package RPIS61.Kirzhaev.wdad;
+
+public class Application {
+    public static void main(String[] args){
+        System.out.println("I’m Dmitry \"Mechanic of AutoVAZ\" Kirzhaev, and I’m not a monkey");
+    }
+}

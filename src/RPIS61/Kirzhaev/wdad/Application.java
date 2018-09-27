8ffa60037eebd0ef5bcff5dc51fe71b0959e943b
Mitrichone <dmitry.kirzhaev@gmail.com>
2018-09-28 01:13:47 +0400

    Added entry point

--- src/RPIS61/Kirzhaev/wdad/Application.java
+++ src/RPIS61/Kirzhaev/wdad/Application.java
@@ -2,6 +2,7 @@ package RPIS61.Kirzhaev.wdad;
 
 public class Application {
     public static void main(String[] args){
+        //кажется я назвал изменения не так как в задании...
         System.out.println("I’m Dmitry \"Mechanic of AutoVAZ\" Kirzhaev, and I’m not a monkey");
     }
 }
